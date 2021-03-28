dadf45b1a44a2a00ad56b0ed3194563a15725302
Kirill Leychenok <starkeiman@yandex.ru>
2021-03-28 14:19:46 +0300

    Init commit

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  getHello(@UploadedFile() file: Express.Multer.File): Express.Multer.File {
+    console.log(file);
+    return file;
+  }
+}
