603d577ec976468fea43aadab78ab5d411c4effe
Kirill Leychenok <starkeiman@yandex.ru>
2021-04-01 20:23:22 +0300

    Added: swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { config } from 'dotenv';
+import { join } from 'path';
 
 import { AppModule } from './app.module';
 
@@ -9,10 +10,15 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   config();
   app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   const documentBuilder = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Instagram')
-    .setDescription('final task')
+    .setDescription(
+      `To get image url concat ${
+        process.env.HOST_URL || 'host url'
+      } and {fileName}`,
+    )
     .setVersion('1.0')
     .build();
   const doc = SwaggerModule.createDocument(app, documentBuilder);
